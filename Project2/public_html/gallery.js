7b1f552ed269288c414e3ca780197c495b73ba76
ArteriusDayne <zakafein@hotmail.com>
2016-03-31 01:26:28 -0400

    Part 2 + 3
    Part 2 and 3 done.  Need to finish part 4, and part 5 should be working
    when that's done.

--- Project2/public_html/gallery.js
+++ Project2/public_html/gallery.js
@@ -32,16 +32,48 @@ function animate() {
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
+//Allows the clickity clackity of the arrow buttons
+function getQueryParams(qs) {
+    qs = qs.split("+").join(" ");
+    var params = {},
+            tokens,
+            re = /[?&]?([^=]+)=([^&]*)/g;
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])]
+                = decodeURIComponent(tokens[2]);
+    }
+    return params;
+}
+var $_GET = getQueryParams(document.location.search);
+
 function swapPhoto() {
     //Add code here to access the #slideShow element.
     //Access the img element and replace its source
     //with a new image from your images array which is loaded 
     //from the JSON string
+
+    //Makes sure it loops the images
+    if (mCurrentIndex > mImages.length - 1) {
+        mCurrentIndex = 0;
+    } else if (mCurrentIndex < 0) {
+        mCurrentIndex = mImages.length - 1;
+
+    }
+    console.log(mCurrentIndex)
+
+    //changes html to add image descriptions
+    $('#slideShow .photoHolder img').attr('src', mImages[mCurrentIndex].imgPath);
+    $('#slideShow .details .location').text("Location: " + mImages[mCurrentIndex].imgLocation);
+    $('#slideShow .details .description ').text("Description: " + mImages[mCurrentIndex].description);
+    $('#slideShow .details .date ').text("Date: " + mImages[mCurrentIndex].date);
+
+    //console.log(mImages[0].imgPath);
     console.log('swap photo');
+    mCurrentIndex++;
 }
 
 // Counter for the mImages array
-var mCurrentIndex = 0;
+var mCurrentIndex = 1;
 
 // XMLHttpRequest variable
 var mRequest = new XMLHttpRequest();
@@ -54,31 +86,14 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mURL = "images.json";
 
-mRequest.onreadystatechange = function () {
 
-    if (mRequest.readyState === 4 && mRequest.status === 200) {
-        try {
-            mJson = JSON.parse(mRequest.responseText);
-            console.log(mJson);
-        } catch (err) {
-            console.log(err.message);
-        }
-    }
-};
-
-mRequest.open("GET", mURL, true);
-mRequest.send();
-
-$.getJSON(mURL, function (data) {
-    $.each(data.items, function (f) {
-        b = GalleryImage(f.imgLocation, f.description, f.date, f.imgPath);
-        mImages.push(b);
-        console.log(mImages);
-    });
-});
-
+//sets to images.json by default
+if ($_GET["json"] === "extra.json") {
+    var mUrl = "extra.json";
+} else {
+    var mUrl = "images.json";
+}
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
@@ -95,23 +110,56 @@ $(document).ready(function () {
     // This initially hides the photos' metadata information
     $('.details').eq(0).hide();
 
+    $("img.moreIndicator").click(function () {
+        if ($(this).hasClass("rot90")) {
+            $(this).removeClass("rot90").addClass("rot270");
+            $("div.details").fadeToggle("slow", "linear");
+        } else {
+            $(this).removeClass("rot270").addClass("rot90");
+            $("div.details").fadeToggle("slow", "linear");
+        }
+    });
+
+    $("#nextPhoto").css({"position": "absolute", "right": "0"});
+
+    $("#nextPhoto").click(function () {
+        swapPhoto()
+    });
+
+    $("#prevPhoto").click(function () {
+        mCurrentIndex = mCurrentIndex - 2;
+        swapPhoto()
+    });
+
 });
 
 window.addEventListener('load', function () {
-$.getJSON(mURL, function (data) {
-    $.each(data.items, function (f) {
-        b = GalleryImage(f.imgLocation, f.description, f.date, f.imgPath);
-        mImages.push(b);
-        console.log(mImages);
-    });
-});
+
     console.log('window loaded');
 
 }, false);
 
-function GalleryImage(location, description, date, img) {
-    this.location = location;
+//Galley Image Object Constructor
+function GalleryImage(imgPath, imgLocation, description, date) {
+    this.imgPath = imgPath;
+    this.imgLocation = imgLocation;
     this.description = description;
     this.date = date;
-    this.img = img;
+
 }
+
+//Creates Galley Image Objects from JSON file, pushes objects into mImages array
+function reqListener() {
+    console.log(JSON.parse(this.responseText));
+    var mJson = JSON.parse(this.responseText);
+    for (var i = 0; i < mJson.images.length; i++) {
+        var current = mJson.images[i];
+        var imageDetails = new GalleryImage(current.imgPath, current.imgLocation, current.description, current.date);
+        mImages.push(imageDetails);
+
+    }
+}
+
+mRequest.addEventListener("load", reqListener);
+mRequest.open("GET", mUrl);
+mRequest.send();
